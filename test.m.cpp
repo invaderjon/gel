d621127ad33ff0874090b1ab01402da4191070c2
Jonathan <jonathan.digiacomo@gmail.com>
2015-10-01 16:33:38 -0400

    Add tests and implement auto configure

--- test.m.cpp
+++ test.m.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
+// test.m.cpp
+#include <gtest/gtest.h>
 
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
