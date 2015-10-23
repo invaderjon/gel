fa9695a2eab09aebbc4cdde1d1fae8d525f168f8
Jonathan <jonathan.digiacomo@gmail.com>
2015-10-23 19:28:37 -0400

    Create matrix declarations file.

---
+++ include/gel/math/mat.h
@@ -0,0 +1,92 @@
+// mat.h
+#ifndef GEL_MAT_H
+#define GEL_MAT_H
+#include <gel/math/mat2x2.h>
+#include <gel/math/mat3x3.h>
+#include <gel/math/mat4x4.h>
+#include <gel/math/mat_math.h>
+#include <gel/math/vec.h>
+
+namespace gel
+{
+
+namespace math
+{
+
+template <typename T>
+class TRef2;
+
+template <typename T>
+class TRef3;
+
+template <typename T>
+class TRef4;
+
+template <typename T>
+class TVec2;
+
+template <typename T>
+class TVec3;
+
+template <typename T>
+class TVec4;
+
+template <typename T>
+class TMat2x2;
+
+template <typename T>
+class TMat3x3;
+
+template <typename T>
+class TMat4x4;
+
+/**
+ * Defines a default 2x2 matrix.
+ */
+typedef TMat2x2<float> Mat2;
+
+/**
+ * Defines a double 2x2 matrix.
+ */
+typedef TMat2x2<double> DMat2;
+
+/**
+ * Defines an integer 2x2 matrix.
+ */
+typedef TMat2x2<int> IMat2;
+
+/**
+ * Defines a default 3x3 matrix.
+ */
+typedef TMat3x3<float> Mat3;
+
+/**
+ * Defines a double 3x3 matrix.
+ */
+typedef TMat3x3<double> DMat3;
+
+/**
+ * Defines an integer 3x3 matrix.
+ */
+typedef TMat3x3<int> IMat3;
+
+/**
+ * Defines a default 4x4 matrix.
+ */
+typedef TMat4x4<float> Mat4;
+
+/**
+ * Defines a double 4x4 matrix.
+ */
+typedef TMat4x4<double> DMat4;
+
+/**
+ * Defines an integer 4x4 matrix.
+ */
+typedef TMat4x4<int> IMat4;
+
+} // End nspc math
+
+} // End nspc gel
+
+#endif //GEL_MAT_H
